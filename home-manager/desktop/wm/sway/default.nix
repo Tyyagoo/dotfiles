d00a49ca37e130d0eb21190c14a0cdc874ddc750
Tyyagoo <tyyago.dev@gmail.com>
2022-12-29 18:41:38 -0300

    `sway`: add gaps and border colors

--- home-manager/desktop/wm/sway/default.nix
+++ home-manager/desktop/wm/sway/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, libs, ...}: {
     wayland.windowManager.sway = {
         enable = true;
+
         config = rec {
             modifier = "Mod4";
             terminal = "foot";
@@ -16,6 +17,35 @@
                     xkb_model = "abnt2";
                 };
             };
+
+            gaps = {
+                inner = 10;
+                outer = 4;
+                smartGaps = true;
+            };
+
+            colors = {
+                focused = {
+                    border = "#cba6f7";
+                    background = "#cba6f7";
+                    text = "#cba6f7";
+                    indicator = "#cba6f7";
+                    childBorder = "#cba6f7";
+                };
+
+                unfocused = {
+                    border = "#1e1e2e";
+                    background = "#1e1e2e";
+                    text = "#1e1e2e";
+                    indicator = "#1e1e2e";
+                    childBorder = "#1e1e2e";
+                };
+            };
         };
+
+        # configs for SwayFX
+        # extraConfig = ''
+        #     corner_radius 12
+        # '';
     };
 }
